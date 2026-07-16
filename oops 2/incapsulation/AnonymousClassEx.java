cd26c501934b190f3ea8a2a3e45a0ad9b8493d6e
Gajanan Deshmukh <deshmukhgajananyashwant@gmail.com>
2026-07-17 00:48:50 +0530

    Anonymous class

--- oops 2/incapsulation/AnonymousClassEx.java
+++ oops 2/incapsulation/AnonymousClassEx.java
@@ -1,3 +1,17 @@
-public class AnonymousClassEx {
-    
+interface Greeting {
+    void sayHello();
 }
+
+public class AnonymousClassEx {
+    public static void main(String[] args) {
+        Greeting g = new Greeting() {
+            @Override
+            public void sayHello() {
+                System.out.println("Hello from Anonymous Class!");
+            }
+        };
+        g.sayHello();
+    }
+}
+
+//Anonymous classes are like “quick one-time use classes”. Instead of creating a separate file or class, you define them inline.
--- oops 2/incapsulation/PyramidPattern.java
+++ oops 2/incapsulation/PyramidPattern.java
@@ -1,7 +1,7 @@
 public class PyramidPattern {
     public static void main(String[] args) {
         // Loop for rows
-        for(int i = 1; i <= 5; i++) {
+        for(int i = 5; i >= 1; i--) {
             // Loop for stars in each row
             for(int j = 1; j <= i; j++) {
                 System.out.print("*");
