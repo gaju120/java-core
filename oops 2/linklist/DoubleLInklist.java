4c823fabffdae3a3d42daf4891172d9b3e45fcf7
Gajanan Deshmukh <deshmukhgajananyashwant@gmail.com>
2026-07-28 23:30:49 +0530

    Double linklist and  rapper class

---
+++ oops 2/linklist/DoubleLInklist.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class DoubleLInklist {
+     public static void main(String[] args) {
+      // ArrayList<int> list=new ArrayList<>();
+      
+      Integer  num =100;
+      
+      System.out.println(num.compareTo(100));
+
+      String age ="28";
+      int intage =Integer.parseInt(age);
+
+      System.out.println(age +5);
+      System.out.println(intage +5);
+     }  
+}
---
+++ oops 2/linklist/doublelinklistDemo.java
@@ -0,0 +1,28 @@
+import java.util.Hashmap;
+import java.util.Map;
+
+public class doublelinklistDemo {
+    public static void main(String[] args) {
+        // Declare and initialize the map
+        Map<Integer, String> map = new Hashmap<>();
+
+        // Add key-value pairs
+        map.put(101, "pranav");
+        map.put(102, "ram");
+        map.put(103, "ramesh");
+
+        // Retrieve value by key
+        System.out.println(map.get(102)); // Output: ram
+
+        // Check if key exists
+        System.out.println(map.containsKey(102)); // Output: true
+
+        map.remove(103);
+
+        for(Integer elem :map.keySet()){
+            System.out.println(elem);
+        }
+    }
+}
+
+
